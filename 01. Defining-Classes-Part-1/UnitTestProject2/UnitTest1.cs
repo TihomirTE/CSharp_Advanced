43be2fff9b829772222755526ef9d17ac7e20141
Ivangta <ivanntu8@gmail.com>
2020-02-18 19:19:21 +0200

    Changes made, new test added.

---
+++ 01. Defining-Classes-Part-1/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _01.Defining_Classes_Part_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var battery = new GSMTest();
+
+            //Act
+
+            //Assert
+        }
+    }
+}
